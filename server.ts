587fb7cef31e502a9ec33110941073ab35a302c8
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-02 22:08:35 +0530

    Add basic Express server setup

---
+++ server.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+export const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

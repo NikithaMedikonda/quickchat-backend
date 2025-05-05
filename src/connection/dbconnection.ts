352ffc1752f6e518843423792b158c5dd60b910f
ushasri2645 <ugudikandula@everest.engineering>
2025-05-05 19:22:20 +0530

    Handle Missing env variables using try catch.

--- src/connection/dbconnection.ts
+++ src/connection/dbconnection.ts
@@ -1,25 +1,28 @@
 import { Dialect, Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
 export const SequelizeConnection = () => {
   let sequelize;
-  if (
-    !process.env.DB_DIALECT ||
-    !process.env.DATABASE_URL ||
-    !process.env.DB_HOST 
-  ) {
-    console.error("Environment Variables missing!");
-    return;
-  } else {
-    sequelize = new Sequelize(process.env.DATABASE_URL as string, {
-      host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT as Dialect,
-      logging: false,
-    });
+  try {
+    if (
+      !process.env.DB_DIALECT ||
+      !process.env.DATABASE_URL ||
+      !process.env.DB_HOST
+    ) {
+      throw new Error("Environment Variables are missing!");
+    } else {
+      sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT as Dialect,
+        logging: false,
+      });
+    }
+    return sequelize;
+  } catch (error: any) {
+    throw new Error(error.message);
   }
-  return sequelize;
 };
 
 export const sequelizeInstance = SequelizeConnection();

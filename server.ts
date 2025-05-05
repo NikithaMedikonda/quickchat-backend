6084d5651f05081283f17b4c1e73da20957eea93
ushasri2645 <ugudikandula@everest.engineering>
2025-05-05 19:21:40 +0530

    Handle errors using try catch

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { sequelizeInstance } from "./src/connection/dbconnection";
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 export const app = express();
 const PORT = process.env.PORT;
 
@@ -11,14 +11,23 @@ app.use(express.json());
 app.use(cors());
 
 const startServer = async () => {
-  if(!sequelizeInstance){
-    console.error("Sequelize Instance is not defined!");
-    return;
+  try {
+    if (!sequelizeInstance) {
+      throw new Error("Sequelize instance not found. Please create!");
+    }
+    await sequelizeInstance.authenticate();
+    console.log("Database connected successfully!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(
+        `Error occurred during server startup:, ${error.message}`
+      );
+    } else {
+      throw new Error(`An unknown error occurred.`);
+    }
   }
-  await sequelizeInstance.authenticate();
-  console.log("Database connected successfully!");
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
 };
 startServer();

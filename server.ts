646c64750036c73c62f31e8e53ae6c162d9be899
ushasri2645 <ugudikandula@everest.engineering>
2025-05-05 15:10:35 +0530

    Authenticating sequelize instance created.

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { sequelizeInstance } from "./src/connection/dbconnection";
 
 dotenv.config();
 export const app = express();
@@ -9,9 +10,15 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-const startServer  = async() => {
+const startServer = async () => {
+  if(!sequelizeInstance){
+    console.error("Sequelize Instance is not defined!");
+    return;
+  }
+  await sequelizeInstance.authenticate();
+  console.log("Database connected successfully!");
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
-}
+};
 startServer();

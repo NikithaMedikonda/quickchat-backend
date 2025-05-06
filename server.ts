87ed49bb56c3461c24a41efb0d97617baa032e7c
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-06 16:02:54 +0530

    Use the user routes in the server file

--- server.ts
+++ server.ts
@@ -2,21 +2,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { sequelizeInstance } from "./src/connection/dbconnection";
-
+import userRoutes from "./src/user/user.route";
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 export const app = express();
 const PORT = process.env.PORT;
-
+import { User } from "./src/user/user.model";
 app.use(express.json());
 app.use(cors());
-
+app.use(userRoutes);
 const startServer = async () => {
   try {
     if (!sequelizeInstance) {
       throw new Error("Sequelize instance not found. Please create!");
     }
     await sequelizeInstance.authenticate();
-    console.log("Database connected successfully!");
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });

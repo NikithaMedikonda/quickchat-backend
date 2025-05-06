e82cc6603d60c7eb689439a0e0ea9f02ee39da3e
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-06 15:59:44 +0530

    Create the user and DbUser type

---
+++ src/types/user.ts
@@ -0,0 +1,19 @@
+export type user = {
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  profilePicture?: string ;
+  email?: string ;
+  password: string;
+  isDeleted: boolean;
+};
+export type DbUser = {
+  id: string;
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  profilePicture?: string ;
+  email?: string ;
+  password: string;
+  isDeleted: boolean;
+};

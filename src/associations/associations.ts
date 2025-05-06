c8afbcacd4e250c72dc5d2a533d10e7070ae3874
ushasri2645 <ugudikandula@everest.engineering>
2025-05-06 23:21:00 +0530

    Add many to many relation ship between user and chat

--- src/associations/associations.ts
+++ src/associations/associations.ts
@@ -8,22 +8,34 @@ export const syncAssociations = () => {
   User.hasMany(Chat, { as: "chatInitiated", foreignKey: "chatId" });
   User.hasMany(Chat, { as: "chatReceived", foreignKey: "chatId" });
 
-  Chat.belongsTo(User, { as: "user1", foreignKey: "user1Id" });
-  Chat.belongsTo(User, { as: "user2", foreignKey: "user2Id" });
+  Chat.belongsTo(User, { as: "userA", foreignKey: "userAId" });
+  Chat.belongsTo(User, { as: "userB", foreignKey: "userBId" });
 
   Chat.hasMany(Message, { foreignKey: "chatId" });
   Message.belongsTo(Chat, { foreignKey: "chatId" });
 
-  User.hasMany(Message, { as: "MessagesSent", foreignKey: "senderId" });
-  User.hasMany(Message, { as: "MessagesReceived", foreignKey: "receiverId" });
+  User.hasMany(Message, { as: "messagesSent", foreignKey: "senderId" });
+  User.hasMany(Message, { as: "messagesReceived", foreignKey: "receiverId" });
   Message.belongsTo(User, { as: "sender", foreignKey: "senderId" });
   Message.belongsTo(User, { as: "receiver", foreignKey: "receiverId" });
 
   User.hasMany(Conversation, { foreignKey: "userId" });
   Conversation.belongsTo(User, { foreignKey: "userId" });
 
-  User.hasMany(BlockedUsers, { as: "BlockedUsers", foreignKey: "blocker" });
-  User.hasMany(BlockedUsers, { as: "BlockedBy", foreignKey: "blocked" });
+  User.hasMany(BlockedUsers, { as: "blockedUsers", foreignKey: "blocker" });
+  User.hasMany(BlockedUsers, { as: "blockedBy", foreignKey: "blocked" });
   BlockedUsers.belongsTo(User, { as: "blocked_by", foreignKey: "blocker" });
   BlockedUsers.belongsTo(User, { as: "blocked_whom", foreignKey: "blocked" });
+
+  User.belongsToMany(Chat, {
+    through: Conversation,
+    foreignKey: 'userId',
+    otherKey: 'chatId',
+  });
+  
+  Chat.belongsToMany(User, {
+    through: Conversation,
+    foreignKey: 'chatId',
+    otherKey: 'userId',
+  });
 };

ed07c22122145e9eb0ce661e05d03cce7d135deb
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-05-07 18:06:10 +0530

    Add post api/user route for login API

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "./user.controller";
-import { validateInputFields } from "./user.middleware";
+import { register, login } from "./user.controller";
+import { validateInputFields, validateLoginInputFields} from "./user.middleware";
 
 const router = express.Router();
 
 router.post("/api/users",validateInputFields,register);
-
+router.post("/api/user",validateLoginInputFields, login);
 export default router;

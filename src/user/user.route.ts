31a1de1653eed0fac662b5ad6450f0b7eaed95bb
ushasri2645 <ugudikandula@everest.engineering>
2025-05-12 18:05:34 +0530

    Update deleteAccount import statement

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, update } from "./user.controller";
+import { deleteAccount, login, register, update } from "./user.controller";
 import {
   authenticateToken,
   validateInputFields,

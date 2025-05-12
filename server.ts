77cfc01bcfc0a898adcb2d02981fd81b08bbbc87
ushasri2645 <ugudikandula@everest.engineering>
2025-05-12 12:22:24 +0530

    Rename router to userRouter.

--- server.ts
+++ server.ts
@@ -1,10 +1,9 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { router } from "./src/user/user.route";
-import { sequelizeInstance } from "./src/connection/dbconnection";
 import { syncAssociations } from "./src/associations/associations";
-
+import { sequelizeInstance } from "./src/connection/dbconnection";
+import { userRouter } from "./src/user/user.route";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 export const app = express();
@@ -12,7 +11,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
-app.use(router);
+app.use(userRouter);
 
 const startServer = async () => {
   try {

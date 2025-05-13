196752577988630077e30e0ad15f69f559b039ad
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-13 12:01:19 +0530

    Add checkAuth route

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { login, register } from "./user.controller";
+import { checkAuth, login, register } from "./user.controller";
 import { validateInputFields, validateLoginInputFields } from "./user.middleware";
 
 export const userRouter = express.Router();
 
 userRouter.post("/api/users", validateInputFields, register);
 userRouter.post("/api/user", validateLoginInputFields, login);
+userRouter.post("/api/checkAuth", checkAuth);

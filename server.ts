1d0dd0f9acb098358514352480e137ca33c8be78
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-20 12:41:35 +0530

    Establish socket connection in the server

--- server.ts
+++ server.ts
@@ -1,17 +1,25 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { syncAssociations } from "./src/associations/associations";
+import http from "http";
+import { Server } from "socket.io";
 import { sequelizeInstance } from "./src/connection/dbconnection";
+import { setupSocket } from "./src/socket/socket";
+import { syncAssociations } from "./src/associations/associations";
 import { userRouter } from "./src/user/user.route";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+
 export const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
+
+const server = http.createServer(app);
+const io = new Server(server);
+setupSocket(io);
 
 const startServer = async () => {
   try {
@@ -20,9 +28,15 @@ const startServer = async () => {
     }
     await sequelizeInstance.authenticate();
     console.log("Database connected successfully!");
+
     syncAssociations();
-    console.log("Associations synced successfully.")
-    app.listen(PORT, () => {
+    console.log("Associations synced successfully.");
+    if (process.env.NODE_ENV === "test") {
+      await sequelizeInstance.sync({ alter: true });
+    }
+    console.log("Associations synced successfully.");
+
+    server.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error: unknown) {

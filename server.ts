4d5852a6a9c6e7884d811bb0882f98cb3da8c188
ushasri2645 <ugudikandula@everest.engineering>
2025-05-20 22:26:51 +0530

    Use message router, sync models and listen to app only in dev env.

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { syncAssociations } from "./src/associations/associations";
 import { sequelizeInstance } from "./src/connection/dbconnection";
+import { messageRouter } from "./src/message/message.router";
 import { userRouter } from "./src/user/user.route";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
+app.use(messageRouter);
 
 const startServer = async () => {
   try {
@@ -19,21 +21,15 @@ const startServer = async () => {
       throw new Error("Sequelize instance not found. Please create!");
     }
     await sequelizeInstance.authenticate();
-    console.log("Database connected successfully!");
     syncAssociations();
-    console.log("Associations synced successfully.");
-    await sequelizeInstance.sync({alter:true})
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      throw new Error(
-        `Error occurred during server startup:, ${error.message}`
-      );
-    } else {
-      throw new Error(`An unknown error occurred.`);
+    await sequelizeInstance.sync({ alter: true });
+    if (process.env.NODE_ENV !== "test") {
+      app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+      });
     }
+  } catch (error) {
+    `Error occurred during server startup:, ${error}`;
   }
 };
 startServer();

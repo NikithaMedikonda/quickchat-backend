1f86ce9d03fce3b53684644999a602cb6f625a2b
ushasri2645 <ugudikandula@everest.engineering>
2025-05-21 13:30:51 +0530

    Add message router.

--- server.ts
+++ server.ts
@@ -17,6 +17,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
+app.use(messageRouter);
 
 const server = http.createServer(app);
 const io = new Server(server);

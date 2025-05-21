86a3ac1fc252cc86e4707134756f0f91fcdea1b6
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-05-21 17:00:25 +0530

    Add service for removing  entry from BlockedUsers model

--- src/blockedusers/blocked-users.service.ts
+++ src/blockedusers/blocked-users.service.ts
@@ -11,7 +11,32 @@ export const addBlockedUserEntry = async (
     });
     return blockUserDetails;
   } catch (error) {
-    throw new Error(`Error occurred while blocking user : ${(error as Error).message}`);
+    throw new Error(
+      `Error occurred while blocking user : ${(error as Error).message}`
+    );
   }
 };
 
+export const removeBlockedUserEntry = async (
+  blockerId: string,
+  blockedId: string
+): Promise<void> => {
+  try {
+    const blockedEntry = await BlockedUsers.findOne({
+      where: {
+        blocker: blockerId,
+        blocked: blockedId,
+      },
+    });
+
+    if (!blockedEntry) {
+      throw new Error("Blocked entry not found.");
+    }
+
+    await blockedEntry.destroy();
+  } catch (error) {
+    throw new Error(
+      `Error occurred while unblocking user: ${(error as Error).message}`
+    );
+  }
+};

091444f85b32aefdf98819c8531308b63701c3f0
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-05-21 14:32:03 +0530

    Add service for making entry into BlockedUsers model

---
+++ src/blockedusers/blocked-users.service.ts
@@ -0,0 +1,16 @@
+import { BlockedUsers } from "./blocked-users.model";
+
+export const addBlockedUserEntry = async (
+  blockerId: string,
+  blockedId: string
+): Promise<BlockedUsers> => {
+  try {
+    const blockUserDetails = await BlockedUsers.create({
+      blocker: blockerId,
+      blocked: blockedId,
+    });
+    return blockUserDetails;
+  } catch (error) {
+    throw new Error(`Error occured while blocking user : ${(error as Error).message}`);
+  }
+};

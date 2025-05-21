00fba2e54dc2a8df1a160cd6ad2b062f90f66d51
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-05-21 16:59:21 +0530

    Add unblock user router to express router

--- src/blockedusers/blocked-users.router.ts
+++ src/blockedusers/blocked-users.router.ts
@@ -1,8 +1,19 @@
-import express from 'express'
-import { authenticateToken } from '../user/user.middleware'
-import { executeUserBlock } from './blocked-users.controller';
-
+import express from "express";
+import { authenticateToken } from "../user/user.middleware";
+import {
+  executeUserBlock,
+  executeUserUnblock,
+} from "./blocked-users.controller";
 
 export const blockedUsersRouter = express.Router();
 
-blockedUsersRouter.post('/api/block/users',authenticateToken, executeUserBlock);
+blockedUsersRouter.post(
+  "/api/block/users",
+  authenticateToken,
+  executeUserBlock
+);
+blockedUsersRouter.post(
+  "/api/unblock/users",
+  authenticateToken,
+  executeUserUnblock
+);

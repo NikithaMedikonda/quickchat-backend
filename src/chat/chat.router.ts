23ef18f37ba3993a4402ce0289c0f46033945c4e
ushasri2645 <ugudikandula@everest.engineering>
2025-05-26 09:36:22 +0530

    Add a route to fetch all chats of a user.

--- src/chat/chat.router.ts
+++ src/chat/chat.router.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { authenticateToken } from "../user/user.middleware";
-import { userChats } from "./chat.controller";
+import { getChatsOfUser, userChats } from "./chat.controller";
 
 export const chatRouter = express.Router();
 
 chatRouter.post("/api/users/messages", authenticateToken, userChats);
+chatRouter.post("/api/chats/user", authenticateToken, getChatsOfUser)

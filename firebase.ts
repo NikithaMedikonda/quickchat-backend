db4318b93e1a1abbc04c294d92f04a62558d3ba3
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-09 13:32:52 +0530

    Update the function

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,6 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import fs from "fs";
-import path from "path";
 
-let serviceAccount: ServiceAccount;
-const keyPath = path.resolve(__dirname, "serviceAccountKey.json");
-
-if (fs.existsSync(keyPath)) {
-  const fileContents = fs.readFileSync(keyPath, "utf-8");
-  serviceAccount = JSON.parse(fileContents) as ServiceAccount;
-} else if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON) as ServiceAccount;
-} else {
-  throw new Error("Firebase service account key not found.");
-}
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON || '{}') as ServiceAccount;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });

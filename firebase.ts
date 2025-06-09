642eb5da4485e5489ad5b1ab5141357738170662
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-09 16:24:29 +0530

    Update the firebase configuration file

--- firebase.ts
+++ firebase.ts
@@ -1,20 +1,8 @@
-import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccountLocal from "./serviceAccountKey.json";
+import admin from "firebase-admin";
+import serviceAccountJson from "./serviceAccountKey.json";
+import { ServiceAccount } from "firebase-admin";
 
-let serviceAccount: ServiceAccount;
-
-if (process.env.NODE_ENV === "production") {
-  const rawServiceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT_JSON || "{}"
-  );
-
-  serviceAccount = {
-    ...rawServiceAccount,
-    privateKey: rawServiceAccount.private_key.replace(/\\n/g, "\n"),
-  };
-} else {
-  serviceAccount = serviceAccountLocal as ServiceAccount;
-}
+const serviceAccount = serviceAccountJson as ServiceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

2be0d2532cd9004cf41c1c66f04865f409652e55
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-09 18:38:11 +0530

    Update the firebase configuration file

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,15 @@
-import admin from "firebase-admin";
-import serviceAccountJson from "./serviceAccountKey.json";
-import { ServiceAccount } from "firebase-admin";
-
-const serviceAccount = serviceAccountJson as ServiceAccount;
-
+import admin from 'firebase-admin';
+import fs from 'fs';
+let serviceAccount: admin.ServiceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+} else {
+  const file = fs.readFileSync('./serviceAccountKey.json', 'utf-8');
+  serviceAccount = JSON.parse(file);
+}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-export const messaging = admin.messaging();
+const messaging = admin.messaging();
+export { messaging };
+export default admin;

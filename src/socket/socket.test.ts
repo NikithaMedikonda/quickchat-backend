653ace9aadb0e083dfabc66ffbf9bb57214016fd
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-10 16:51:49 +0530

    Update the socket test

--- src/socket/socket.test.ts
+++ src/socket/socket.test.ts
@@ -90,10 +90,10 @@ describe("Test for socket", () => {
     expect(chat).toBeDefined();
   });
 
-  test("should verify device for check_user_device event", (done) => {
-    const phoneNumber = "+919440058888";
-    const correctDeviceId = "device123";
-    const incorrectDeviceId = "wrongDevice";
+test("should verify device for check_user_device event", (done) => {
+  const phoneNumber = "+919440058888";
+  const correctDeviceId = "device123";
+  const incorrectDeviceId = "wrongDevice";
 
     User.create({
       phoneNumber,
@@ -113,24 +113,23 @@ describe("Test for socket", () => {
       clientA.on("connect", () => {
         clientA.emit("check_user_device", phoneNumber, correctDeviceId);
 
-        clientA.once("user_device_verified", (response1) => {
-          try {
-            expect(response1).toEqual({
-              success: true,
-              message: "Device verified",
-              action: "continue",
-            });
 
-            clientA.emit("check_user_device", phoneNumber, incorrectDeviceId);
+      clientA.once("user_device_verified", (response1) => {
+        expect(response1).toEqual({
+          success: true,
+          message: "Device verified",
+          action: "continue",
+        });
 
-            clientA.once("user_device_verified", (response2) => {
-              try {
-                expect(response2).toEqual({
-                  success: false,
-                  message: "Device mismatch - logged in from another device",
-                  action: "logout",
-                  registeredDeviceId: correctDeviceId,
-                });
+        clientA.emit("check_user_device", phoneNumber, incorrectDeviceId);
+
+        clientA.once("user_device_verified", (response2) => {
+          expect(response2).toEqual({
+            success: false,
+            message: "Device mismatch - logged in from another device",
+            action: "logout",
+            registeredDeviceId: correctDeviceId,
+          });
 
           clientA.emit("check_user_device", "+0000000000", "anyDevice");
 
@@ -147,7 +146,6 @@ describe("Test for socket", () => {
     });
   }).catch(done);
 }, 15000);
-
 
   test("should handle join event, update socket ID and broadcast to other users", (done) => {
     const phoneNumber = "+919440058803";

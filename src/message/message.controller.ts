61e329332c54bb8e3b6fa12be5c884da28699a50
ushasri2645 <ugudikandula@everest.engineering>
2025-06-12 21:52:37 +0530

    Remove console logs

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -84,7 +84,6 @@ export const updateMessageStatus = async (req: Request, res: Response) => {
       res.status(204).end();
       return;
     }
-    console.log("updatedCount", updatedCount);
     res
       .status(200)
       .json({ count: updatedCount, message: "Updated status of messages" });

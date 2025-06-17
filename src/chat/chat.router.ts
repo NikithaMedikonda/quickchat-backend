b9273081fb95eceee79cf9e60b12f01dd195cb27
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-06-17 22:23:11 +0530

    Add /api/user/messages endpoint to fetch user chats and messages

--- src/chat/chat.router.ts
+++ src/chat/chat.router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authenticateToken } from "../user/user.middleware";
-import { getChatsOfUser, getMessagesForSync, userChats } from "./chat.controller";
+import { getChatsOfUser, getDataOfuser, getMessagesForSync, userChats } from "./chat.controller";
 
 export const chatRouter = express.Router();
 
 chatRouter.post("/api/users/messages", authenticateToken, userChats);
 chatRouter.post("/api/chats/user", authenticateToken, getChatsOfUser);
 chatRouter.post("/api/sync",authenticateToken, getMessagesForSync)
+chatRouter.post("/api/user/messages",authenticateToken,getDataOfuser);
